f0ecf027e05cfc068a45b876ab0dbed4388ec509
ralfzzz <azbimanyu@gmail.com>
2023-07-11 20:42:57 +0700

    updater
    shop icon animate

---
+++ index.js
@@ -0,0 +1,9 @@
+// $(document).click(function(){
+//     alert('jquery ready');
+// });
+$('.shop-icon').hide();
+
+$("#shop").click(function(){
+    $(".shop-icon").slideToggle('linear');
+    // console.log('tes');
+  });
