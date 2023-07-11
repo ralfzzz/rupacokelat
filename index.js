2036ff5d7ff5bab1911dca9bb7a4cf96124d91ae
ralfzzz <azbimanyu@gmail.com>
2023-07-11 21:07:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
 // });
 $('.shop-icon').hide();
 
-$("#shop").click(function(){
+$("#shop").click(function(event){
     $(".shop-icon").slideToggle('linear');
+    if (event.currentTarget.id == "shop") {
+        $("#shop").attr("src", "/assets/img/cross.svg").attr("id","shop-cross");
+    } else {
+        $("#shop-cross").attr("src", "/assets/img/shop.svg").attr("id","shop");
+    }
     // console.log('tes');
-  });
+});
+
+// $("#shop-cross").click(function(){
+//     $("#shop-cross").attr("src", "/assets/img/shop.svg").attr("id","shop");
+// });
