a32622be9c97a86f796c9ddf7f8511adda1460e0
ralfzzz <azbimanyu@gmail.com>
2023-07-14 20:53:37 +0700

    updater
    update popup shop-plus & shop icon animations

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ $('.shop-icon').hide();
 $("#shop").click(function(event){
     $(".shop-icon").slideToggle('linear');
     if (event.currentTarget.id == "shop") {
-        $("#shop").attr("src", "/assets/img/cross.svg").attr("id","shop-cross");
+        $("#shop").animate({opacity:'0'},'fast').attr("src", "/assets/img/cross.svg").animate({opacity:'1'},'fast').attr("id","shop-cross");
     } else {
-        $("#shop-cross").attr("src", "/assets/img/shop.svg").attr("id","shop");
+        $("#shop-cross").animate({opacity:'0'},'fast').attr("src", "/assets/img/shop.svg").animate({opacity:'1'},'fast').attr("id","shop");
     }
     // console.log('tes');
 });
 
+
+$('.shop-icon.plus').click(function(event){
+    $(".shop-icon").slideToggle('linear');
+    if (event.currentTarget.id == "shop") {
+        $("#shop").animate({opacity:'0'},'fast').attr("src", "/assets/img/cross.svg").animate({opacity:'1'},'fast').attr("id","shop-cross");
+    } else {
+        $("#shop-cross").animate({opacity:'0'},'fast').attr("src", "/assets/img/shop.svg").animate({opacity:'1'},'fast').attr("id","shop");
+    }
+})
 // $("#shop-cross").click(function(){
 //     $("#shop-cross").attr("src", "/assets/img/shop.svg").attr("id","shop");
 // });
